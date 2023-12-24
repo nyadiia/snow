c685165306eb84c3f524897ee24255798f13f440
nyadiia <nyadiia@pm.me>
2023-12-24 15:34:00 -0600

    updated locale

--- hosts/system.nix
+++ hosts/system.nix
@@ -97,8 +97,20 @@
   boot.kernelPackages = pkgs.linuxPackages_latest;
 
   time.timeZone = "America/Chicago";
-  i18n.defaultLocale = "en_US.UTF-8";
-
+  i18n = {
+    defaultLocale = "en_US.UTF-8";
+    extraLocaleSettings = {
+      LC_ADDRESS = "en_US.UTF-8";
+      LC_IDENTIFICATION = "en_US.UTF-8";
+      LC_MEASUREMENT = "en_US.UTF-8";
+      LC_MONETARY = "en_US.UTF-8";
+      LC_NAME = "en_US.UTF-8";
+      LC_NUMERIC = "en_US.UTF-8";
+      LC_PAPER = "en_US.UTF-8";
+      LC_TELEPHONE = "en_US.UTF-8";
+      LC_TIME = "en_US.UTF-8";
+    };
+  }
   # Configure keymap in X11
   services.xserver.layout = "us";
 
